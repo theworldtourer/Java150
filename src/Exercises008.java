e383dbd7689906727bd290d8ad51ee9162c03f3f
tolgabalkan <tolgabalkan94@gmail.com>
2022-09-03 23:35:54 +0300

    Initial commit

---
+++ src/Exercises008.java
@@ -0,0 +1,9 @@
+public class Exercises008 {
+    public static void first(){
+        System.out.println("" +
+                "   J    a   v     v  a                                                  \n" +
+                "   J   a a   v   v  a a                                                 \n" +
+                "J  J  aaaaa   V V  aaaaa                                                \n" +
+                " JJ  a     a   V  a     a");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Exercises007.first();
+        Exercises008.first();
     }
 }
