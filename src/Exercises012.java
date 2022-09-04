b50d079ea18556d318f5b5038bbf4f55365eaa64
tolgabalkan <tolgabalkan94@gmail.com>
2022-09-04 17:54:51 +0300

    Initial commit

---
+++ src/Exercises012.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercises012 {
+    public static void first(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input first number:");
+        double valFirst = scanner.nextDouble();
+        System.out.print("Input second number:");
+        double valSecond = scanner.nextDouble();
+        System.out.print("Input third number:");
+        double valThird = scanner.nextDouble();
+
+        System.out.println("Average:"+ (valFirst+valSecond+valThird)/3.0);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Exercises011.first();
+        Exercises012.first();
     }
 }
