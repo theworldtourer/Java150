bfa3b0eab16adf27c19ddf0d750a277a50383487
tolgabalkan <tolgabalkan94@gmail.com>
2022-09-10 21:05:44 +0300

    Initial commit

---
+++ src/Exercise015.java
@@ -0,0 +1,20 @@
+public class Exercise015 {
+    public static void first(){
+        /*
+        int i = 1;
+        int a = i;
+        System.out.println(a + i);
+        */
+
+        //swap
+        int val1 = 20;
+        int val2 = 10;
+        int temp;
+
+        temp = val1;
+        val1 = val2;
+        val2 = temp;
+        System.out.println("val1: "+val1 + " val2: " +val2);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Exercise013.first();
+        Exercise015.first();
     }
 }
