808a236c5d68aec6e8cd2b77362645bb470563d7
tolgabalkan <tolgabalkan94@gmail.com>
2022-09-10 21:11:52 +0300

    Initial commit

---
+++ src/Exercise016.java
@@ -0,0 +1,11 @@
+public class Exercise016 {
+    public static void first(){
+        System.out.println("" +
+                " +\"\"\"\"\"+                                                 \n" +
+                "[| o o |]                                                \n" +
+                " |  ^  |                                                 \n" +
+                " | '-' |                                                 \n" +
+                " +-----+");
+
+    }
+}
---
+++ src/Exercise017.java
@@ -0,0 +1,5 @@
+public class Exercise017 {
+    public static void first(){
+
+    }
+}
